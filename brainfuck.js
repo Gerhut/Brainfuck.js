1cda9def0f08a6139084993df34bebb51484b379
Gerhut <Gerhut@GMail.com>
2014-03-25 18:11:00 +0800

    Modify it to compiler mode

--- brainfuck.js
+++ brainfuck.js
@@ -4,93 +4,77 @@
 
   var aryProto = Array.prototype
 
-  function pass() {}
-
   function isUndef(x) {
     return typeof x === 'undefined'
   }
 
   function Memory(input) {
     input = input || ''
-    this.length = 0
     this.ptr = 0
     this.input = aryProto.slice.call(input)
     this.output = []
   }
 
-  var symbolFunc = {
-    '<': function () {
+  Memory.prototype = {
+    left: function () {
       this.ptr = (this.ptr - 1) % MEMORY_LENGTH
     },
-    '>': function () {
+    right: function () {
       this.ptr = (this.ptr + 1) % MEMORY_LENGTH
     },
-    '+': function () {
+    add: function () {
       var value = this[this.ptr]
       if (isUndef(value))
         this[this.ptr] = 1
       else
         this[this.ptr] = (value + 1) % UNIT_LENGTH
     },
-    '-': function () {
+    minus: function () {
       var value = this[this.ptr]
       if (isUndef(value))
         this[this.ptr] = -1
       else
         this[this.ptr] = (value - 1) % UNIT_LENGTH
     },
-    '.': function () {
+    write: function () {
       this.output.push(String.fromCharCode(this[this.ptr]))
     },
-    ',': function () {
+    read: function () {
       this[this.ptr] = this.input.shift().charCodeAt(0) % UNIT_LENGTH
     },
-    '[': function () {
-      return (this[this.ptr] === 0)
-    },
-    ']': function () {
+    notZero: function () {
       return (this[this.ptr] !== 0)
     }
   }
 
-  function calculateLoop(source) {
-    var loopStart = []
-    var loop = {}
-
-    aryProto.forEach.call(source, function (code, index) {
-      if (code === '[') {
-        loopStart.push(index)
-      } else if (code === ']') {
-        var start = loopStart.pop()
-        if (isUndef(start)) {
-          throw new Error('"[" not found to match "]" at ' + i)
-        }
-        loop[start] = index
-        loop[index] = start
-      }
-    })
-
-    if (loopStart.length > 0) {
-      throw new Error('"]" not found to match "[" at ' + loopStart.join(', '))
-    }
-
-    return loop
-  }
-
   function compile(source) {
-    var loop = calculateLoop(source)
-    var funcs = aryProto.map.call(source, function (code) {
-      return symbolFunc[code] || pass
-    })
-    return function (input) {
-      var mem = new Memory(input)
-      for (var i = 0, l = funcs.length; i < l; i++) {
-        if(funcs[i].call(mem)) {
-          i = loop[i]
-        }
+    var header = 'var Memory = this;'
+               + 'return function (input) {'
+               + 'var mem = new Memory(input);'
+    var body = aryProto.map.call(source, function (symbol) {
+      switch(symbol) {
+        case '<':
+          return 'mem.left();'
+        case '>':
+          return 'mem.right();'
+        case '+':
+          return 'mem.add();'
+        case '-':
+          return 'mem.minus();'
+        case ',':
+          return 'mem.read();'
+        case '.':
+          return 'mem.write();'
+        case '[':
+          return 'while (mem.notZero()) {'
+        case ']':
+          return '}'
+        default:
+          return ''
       }
-      return mem.output.join('')
-    }
+    }).join('')
+    var footer = 'return mem.output.join("");}'
+    return Function(header + body + footer).call(Memory)
   }
 
   if (typeof module === 'object' && module.exports) { // node.js
